57047bc6ad84ac345e023d44edff904af31ccf4f
SelOT7 <selamawitsel2@gmail.com>
2023-08-15 09:51:45 -0700

    101-stats.py

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,28 +1,40 @@
 #!/usr/bin/python3
+"""
+Module for log parsing scripts.
+"""
+
 import sys
 
-total_size = 0
-status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
-line_count = 0
+if __name__ == "__main__":
+    size = [0]
+    codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 
-try:
-    for line in sys.stdin:
-        line_count += 1
-        tokens = line.split()
-        total_size += int(tokens[-1])
+    def check_match(line):
+        '''Checks for regexp match in line.'''
         try:
-            status_codes[int(tokens[-2])] += 1
-        except KeyError:
+            line = line[:-1]
+            words = line.split(" ")
+            size[0] += int(words[-1])
+            code = int(words[-2])
+            if code in codes:
+                codes[code] += 1
+        except ValueError:
             pass
-        if line_count % 10 == 0:
-            print("File size: {}".format(total_size))
-            for k in sorted(status_codes.keys()):
-                if status_codes[k] != 0:
-                    print("{}: {}".format(k, status_codes[k]))
-        sys.stdout.flush()
 
-except KeyboardInterrupt:
-    print("File size: {}".format(total_size))
-    for k in sorted(status_codes.keys()):
-        if status_codes[k] != 0:
-            print("{}: {}".format(k, status_codes[k]))
+    def print_stats():
+        '''Prints accumulated statistics.'''
+        print("File size: {}".format(size[0]))
+        for k in sorted(codes.keys()):
+            if codes[k]:
+                print("{}: {}".format(k, codes[k]))
+    i = 1
+    try:
+        for line in sys.stdin:
+            check_match(line)
+            if i % 10 == 0:
+                print_stats()
+            i += 1
+    except KeyboardInterrupt:
+        print_stats()
+        raise
+    print_stats()
